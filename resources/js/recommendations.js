8107d553cdd316fb92778dc9c56e8d0eff680429
BDrobniuch <43449396+bdrobniuch@users.noreply.github.com>
2021-08-01 18:28:48 +0200

    new recomendations and flex for recomendations

--- resources/js/recommendations.js
+++ resources/js/recommendations.js
@@ -5,18 +5,25 @@ var interval = 1000;
 var myVar = setInterval(myTimer, 1000);
 
 var chromaticScale = [
-    '"I love how you lead the training it super and it is perfectly prepared in terms of form"',
-    '"Great training, one of the best I have participated in!"',
-    '"Thank you for the openness of the lecturer and nice time :)"',
-    '"Great training, a lot of knowledge"',
-    '"A great way to conduct online training - breakeout rooms, working together in groups. A very thoughtful way to conduct workshops. Orderly material, logically arranged. The whole thing was dynamic and very interesting. Great that there was more practice than theory."',
-    '"Very positive approach, great involvement of trainers"',
-    '"Interesting approach (I liked not only the good approach to recruitment, but also the part about harmful techniques)"',
-    '"Great materials and tools used for training"',
-    '"THANK YOU VERY MUCH <3 <3 <3 you are great!"',
-    '"They should send everyone from our company to your training :)"',
-    '"I like the use of mindmap to analyze the backlog. Respect for making that powerpoint. A good script."'
-
+    '“I love how you lead the training it super and it is perfectly prepared in terms of form”',
+    '“Great training, one of the best I have participated in!”',
+    '“Thank you for the openness of the lecturer and nice time :)”',
+    '“Great training, a lot of knowledge”',
+    '“Very positive approach, great involvement of trainers”',
+    '“Interesting approach (I liked not only the good approach to recruitment, but also the part about harmful techniques)”',
+    '“Great materials and tools used for training”',
+    '“THANK YOU VERY MUCH <3 <3 <3 you are great!”',
+    '“They should send everyone from our company to your training :)”',
+    '“I like the use of mindmap to analyze the backlog. Respect for making that powerpoint. A good script.”',
+    '“I didn’t know you can work in that way remotely great worskop!”',
+    '“I love that this workshop was so practical not just boring theory and I loved the conversations with others”',
+    '“It was fun do programming together”',
+    '“this was the best ALE Meetup for me”',
+    '“I’d love to try this out in my own team now”',
+    '“Everyone could talk, share their experience and discuss insights NICE!”',
+    '“I liked how we used Miro and the remote IDE tin our discussions”',
+    '“A great way to conduct online training - breakeout rooms, working together in groups. A very thoughtful way to conduct workshops. Orderly material, logically arranged. The whole thing was dynamic and very interesting. Great that there was more practice than theory.”'
+    
 ]
 
 var notePool = [];
